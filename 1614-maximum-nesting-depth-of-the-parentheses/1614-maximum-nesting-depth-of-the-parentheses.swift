62fc21c950597dc86a4001034601857e474c55ce
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-05-01 16:20:59 +0900

    Time: 0 ms (100.00%), Space: 14.2 MB (57.69%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.swift
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.swift
@@ -1,17 +1,18 @@
 class Solution {
     func maxDepth(_ s: String) -> Int {
         var depth : Int = 0
-        var maxDepth: [Int] = [0]
+        var maxD : Int = 0
         for str in s {
             if str == "(" {
                 depth += 1
             }
             else if str == ")" {
-                maxDepth.append(depth)
+                if maxD < depth {
+                    maxD = depth
+                }
                 depth -= 1
             }
         }
-        guard let result = maxDepth.max() else { return 0 }
-        return result        
+        return maxD        
     }
 }
