95674b37a6c409dab6d5a6a925c75e9dbbd614a7
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-05-01 17:39:45 +0900

    Time: 4 ms (66.67%), Space: 14.3 MB (16.67%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.swift
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.swift
@@ -3,27 +3,25 @@ class Solution {
         var resultStudents: [Int] = students
         var resultSandwich: [Int] = sandwiches
         var count = 0
-        while true {
+        
+        while !resultSandwich.isEmpty {
             if resultSandwich[0] == resultStudents[0] {
                 resultSandwich.remove(at: 0)
                 resultStudents.remove(at: 0)
                 count = 0
-                if resultSandwich.isEmpty {
-                    break
-                }
+               
             }
             else {
                 resultStudents.append(resultStudents[0])
-                resultStudents.remove(at: 0)          
-                
+                resultStudents.remove(at: 0)
+            
                 count += 1
                 
                 if count > resultSandwich.count {
                     break
                 }
-            }           
+            }
         }
         return resultSandwich.count
-
     }
 }
