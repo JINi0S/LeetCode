387a3474825306df7bcfd43055c49e1fba88a711
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-05-07 23:33:46 +0900

    Time: 16 ms (8.63%), Space: 13.9 MB (71.98%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.swift
@@ -0,0 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        if list1 == nil { return list2 }
+        if list2 == nil { return list1 }
+
+        var result: ListNode? = ListNode()
+        var tmp = result
+        
+        var l1 = list1
+        var l2 = list2
+        
+        while l1 != nil && l2 != nil {
+            if l1!.val >= l2!.val { 
+                tmp!.next = l2!
+                l2 = l2!.next
+                tmp = tmp!.next
+            }
+            else { 
+                tmp!.next = l1!
+                l1 = l1!.next
+                tmp = tmp!.next
+            }
+            if l1 == nil {
+                tmp!.next = l2
+            } 
+            else if l2 == nil{
+                tmp!.next = l1
+            }
+        }
+        return result!.next
+    }
+    
+    
+    
+    
+    
+    
+    
+    func mergeTwoLists1(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        guard list1 != nil, list2 != nil else {
+            if list1 == nil && list2 == nil { return nil }
+            else if list1 == nil { return list2 }
+            else { return list1 }
+        }
+        
+        var l1 = list1, l2 = list2
+        var result: ListNode? = ListNode()
+        var tmp: ListNode? = result
+                              
+        while l1 != nil && l2 != nil {
+            if l1!.val >= l2!.val {
+                tmp?.next = l2
+                tmp = tmp?.next
+                l2 = l2?.next
+            } else {
+                tmp?.next = l1
+                tmp = tmp?.next
+                l1 = l1?.next
+            }
+        }
+        
+        if l1 == nil {
+            tmp?.next = l2
+        } else {
+            tmp?.next = l1
+        }
+        
+        return result?.next
+    }
+    
+}
