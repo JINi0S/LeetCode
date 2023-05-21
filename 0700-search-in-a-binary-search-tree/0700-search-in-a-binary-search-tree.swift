3039e55aa8d88cf04dd0a99f94e84f6221bd16c6
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-05-21 20:47:18 +0900

    Time: 163 ms (35.45%), Space: 14.9 MB (26.36%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.swift
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        guard let head = root else { return nil }
+        if head.val == val { 
+            return head 
+        }
+        else if head.val > val {
+            return searchBST(head.left, val)
+        }
+        else {
+            return searchBST(head.right, val)
+        }
+    }
+}
