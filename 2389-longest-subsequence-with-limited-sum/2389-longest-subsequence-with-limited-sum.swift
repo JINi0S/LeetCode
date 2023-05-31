f9e8afed2bdc7aa3439c0dd236b9cb0ed065fd0a
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-05-31 21:36:53 +0900

    Time: 90 ms (24.00%), Space: 14.1 MB (88.00%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.swift
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.swift
@@ -1,17 +1,15 @@
 class Solution {
     func answerQueries(_ nums: [Int], _ queries: [Int]) -> [Int] {
         let num = nums.sorted()
-        var prefixSum: [Int] = [0]
-        
-         // 누적 합 계산
+        var sum: [Int] = [0]
+        var result: [Int] = []
+
         for n in num {
-            prefixSum.append(prefixSum.last! + n)
+            sum.append(sum.last! + n)
         }
         
-        var result: [Int] = []
-        
         for query in 0..<queries.count {
-            let count = find(prefixSum, queries[query], 0, prefixSum.count-1)
+            let count = find(sum, queries[query], 0, sum.count-1)
             result.append(count-1)
         }
         return result
