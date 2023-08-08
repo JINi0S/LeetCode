70d257cb5794d41cc9f256ed376a4857273833ca
LeeJinHee <100195563+JINi0S@users.noreply.github.com>
2023-08-08 16:45:24 +0900

    Time: 4 ms (46.15%), Space: 13.7 MB (23.08%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.swift
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.swift
@@ -1,15 +1,10 @@
 class Solution {
     func getMaximumGenerated(_ n: Int) -> Int {
-        var cache: [Int] = Array(repeating: 0, count: n+1)
         if n < 2 { return n }
-        cache[0] = 0
+        var cache: [Int] = Array(repeating: 0, count: n+1)
         cache[1] = 1
         for i in 2...n {
-            if i % 2 == 0 {
-                cache[i] = cache[i/2]
-            } else {
-                cache[i] = cache[i/2] + cache[i/2+1]
-            }
+            cache[i] = cache[i/2] + (i % 2 == 0 ? 0 : cache[i/2+1])
         }
         return cache.max()!
     }
