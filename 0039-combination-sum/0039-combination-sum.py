c48e9b90c2f63fab5f77b168cbaeb14e542f0724
LeeJinHee <wlsgml8847@kakao.com>
2024-12-01 23:30:25 +0900

    Time: 14 ms (35.49%), Space: 17.3 MB (5.10%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -4,23 +4,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         
-        def findCombi(now: [int], ableList: [int]):
-            for able in ableList:
-                tmp_sum = sum(now)+able
-                
-                if tmp_sum == target:
-                    ans.append(tuple(sorted(now[1:]+[able])))
-                    return
-                elif tmp_sum < target:
-                    findCombi(now+[able], ableList)
-                elif tmp_sum > target:
-                    return
-            
-        candidates.sort()
-        for i in range(len(candidates)):
-            ableList = candidates[i:]
-            findCombi([0], ableList)
+        def dfs(i: int, sumV: int, arr: [int]):
+            if sumV == target:
+                ans.append(arr)
+                return
+            elif i == len(candidates) or sumV > target:
+                return
+            elif sumV < target:
+                dfs(i, sumV+candidates[i], arr+[candidates[i]])
+                dfs(i+1, sumV, arr)
 
-        unique_ans = list(map(list, set(ans)))  # 중복 제거 후 다시 리스트로 변환
-        return unique_ans
+        dfs(0,0,[])
+
+        return ans
        
+        
