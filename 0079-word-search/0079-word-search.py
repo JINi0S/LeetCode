82fa0d95b9b9ded4d8cc9d44cca36cfd0cb48528
LeeJinHee <wlsgml8847@kakao.com>
2024-12-02 18:42:52 +0900

    Time: 5283 ms (22.50%), Space: 17.3 MB (5.43%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -7,35 +7,28 @@ class Solution:
             return 0 <= x <len(board) and 0 <= y < len(board[0])
         
         
-        def dfs(x, y, idx, visited) -> bool:
+        def dfs(x, y, idx) -> bool:
             if idx == len(word) - 1:  # 마지막 글자까지 찾았을 경우
                 return True
 
             for d in range(4):
                 nx = x + direct[d][0]
                 ny = y + direct[d][1]
-
-                if not inRange(nx, ny) or (nx, ny) in visited:
+                if not inRange(nx, ny) or word[idx+1] != board[nx][ny]:
                     continue
+                
+                temp, board[x][y] = board[x][y], "v"  # 임시 방문 표시
+                if dfs(nx,ny,idx+1):
+                    return True
+                board[x][y] = temp # 방문 해제
+                
+            return False
+        
 
-                nextWord = word[idx+1]
-                if nextWord == board[nx][ny]:
-                    visited.add((nx,ny))
-                    if dfs(nx,ny,idx+1,visited):
-                        return True
-                    visited.remove((nx,ny))
-            return False
-        
-        
-        if len(word) > len(board) * len(board[0]):
-            return False
-        
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j] == word[0]:
-                    visited = set()
-                    visited.add((i, j))
-                    if dfs(i, j, 0, visited):
+                    if dfs(i, j, 0):
                         return True
 
         return False
