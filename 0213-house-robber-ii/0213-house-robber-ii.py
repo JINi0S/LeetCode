255a4b2f7fc82e05be917dab914724fcbd00d1d5
LeeJinHee <wlsgml8847@kakao.com>
2024-12-10 20:17:03 +0900

    Time: 0 ms (100.00%), Space: 17.2 MB (10.81%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+        처음이 true면 리스트[n] = false > [:-1]
+        처음이 false면 리스트[n]은 상관없음 [1:]
+        """
+        def find(nums) -> int:
+            n = len(nums)
+            dp = [0 for i in range(n)]
+            for i in range(0, 2):
+                dp[i] = max(list(nums[0:i+1]))
+            for i in range(2, n):
+                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+            return dp[n-1]
+        
+        n = len(nums)
+        if n <= 3:
+            return max(nums)
+        
+        return max(find(nums[1:]), find(nums[:n-1]))
+        
+            
+            
