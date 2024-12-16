3d33ae82a927e09021ed3ae45cd09bb35cec2adf
LeeJinHee <wlsgml8847@kakao.com>
2024-12-16 22:09:10 +0900

    Time: 79 ms (66.03%), Space: 15.5 MB (19.76%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def minDistance(self, word1, word2):
         m, n = len(word1), len(word2)
-        dp = [list(range(n+1))]+[[r+1]+[0]*n for r in range(m)]
+        dp = [list(range(n+1))] + [[r+1]+[0]*n for r in range(m)]
         
         for i in range(m):
             for j in range(n):
@@ -9,4 +9,5 @@ class Solution(object):
                     dp[i+1][j+1] = dp[i][j]  
                 else:
                     dp[i+1][j+1] = min(dp[i][j], dp[i+1][j], dp[i][j+1]) + 1
+       
         return dp[m][n]
